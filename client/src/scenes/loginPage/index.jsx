a5588190a2fd91f1b2c6cb02cf9dccdfc82a14ff
Karim-Aridj <abdelkarim.ardj@gmail.com>
2023-01-01 18:05:24 +0100

    setting up register with loginpage and form

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,5 +1,37 @@
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/system";
+import Form from "./Form";
 const LoginPage = () => {
-    return (<div>loginpage</div>);
+    const theme = useTheme();
+    const isNonMobileScreens = useMediaQuery("(min-width: 1000p)");
+    return <Box>
+        <Box 
+        width = "100%" 
+        backgroundColor={theme.palette.background.alt}
+        p= "1rem 6%" 
+        textAlign="center"
+        >
+        <Typography
+          fontWeight="bold"
+          fontSize="32px"
+          color="primary"
+        >
+          ZSocial
+        </Typography>
+        </Box>
+        <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius= "1.5rem"
+        backgroundColor={theme.palette.background.alt}
+        >
+            <Typography fontWeight= "500" variant="h5" sx={{mb:"1.rem"}}>
+                Welcome to ZSocial, The Social Media for all Generations!
+            </Typography>
+            <Form />
+        </Box>
+    </Box>;
 };
+
 
 export default LoginPage
--- client/src/scenes/navBar/index.jsx
+++ client/src/scenes/navBar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          ZSocial
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
