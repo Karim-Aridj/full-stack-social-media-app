05cc3c5f3a831165625abdf437d3ef78adcbc89f
Karim-Aridj <abdelkarim.ardj@gmail.com>
2023-01-01 18:56:34 +0100

    finished setting up form

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -19,11 +19,11 @@ import { ErrorSharp, Password } from "@mui/icons-material";
 
 const registerSchema = yup.object().shape({
   firstName: yup.string().required("required"),
-  lastName: yup.string().required("required"),
+  email: yup.string().required("required"),
   email: yup.string().email("invalid email").required("required"),
   Password: yup.string().required("required"),
   location: yup.string().required("required"),
-  occupation: yup.string().required("required"),
+  email: yup.string().required("required"),
   picture: yup.string().required("required"),
 });
 
@@ -34,11 +34,11 @@ const loginSchema = yup.object().shape({
 
 const initialValuesRegister = {
   firstName: "",
-  lastName: "",
+  email: "",
   email: "",
   Password: "",
   location: "",
-  occupation: "",
+  email: "",
   picture: "",
 };
 
@@ -73,32 +73,113 @@ const Form = () => {
         handleSubmit,
         setFieldValue,
         resetForm,
-      }) => <form onSubmit={handleSubmit}>
-            <Box
-            display = "grid"
-            gap = "30px"
-            gridTemplateColumns= "repeat(4, minmax(0, 1fr))"
+      }) => (
+        <form onSubmit={handleSubmit}>
+          <Box
+            display="grid"
+            gap="30px"
+            gridTemplateColumns="repeat(4, minmax(0, 1fr))"
             sx={{
-                  "& > div": {gridColumn: isNonMobile ? undefined : "span 4"},
+              "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
             }}
-            >
-                  {isRegister && (
-                        <>
-                        <TextField 
-                        label = "First Name"
-                        onBlur={handleBlur}
-                        onChange={handleChange}
-                        value={values.firstName}
-                        name= "firstName"
-                        error={Boolean(touched.firstName) && Boolean(errors.firstName)}
-                        helperText={touched.firstName && ErrorSharp.firstName}
+          >
+            {isRegister && (
+              <>
+                <TextField
+                  label="First Name"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.firstName}
+                  name="firstName"
+                  error={
+                    Boolean(touched.firstName) && Boolean(errors.firstName)
+                  }
+                  helperText={touched.firstName && ErrorSharp.firstName}
+                  sx={{ gridColumn: "span 2" }}
+                />
+                <TextField
+                  label="Location"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.location}
+                  name="location"
+                  error={Boolean(touched.location) && Boolean(errors.location)}
+                  helperText={touched.location && ErrorSharp.location}
+                  sx={{ gridColumn: "span 4" }}
+                />
+                <TextField
+                  label="email"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.email}
+                  name="email"
+                  error={Boolean(touched.email) && Boolean(errors.email)}
+                  helperText={touched.email && ErrorSharp.email}
+                  sx={{ gridColumn: "span 4" }}
+                />
+                <Box
+                  gridColumn="span 4"
+                  border={`1px solid ${palette.neutral.medium}`}
+                  borderRadius="5px"
+                  p="1rem"
+                >
+                  <Dropzone
+                    acceptedFiles=".jpg,.jpeg,.png"
+                    multiple={false}
+                    onDrop={(acceptedFiles) =>
+                      setFieldValue("picture", acceptedFiles[0])
+                    }
+                  >
+                    {({ getRootProps, getInputProps }) => (
+                      <Box
+                        {...getRootProps()}
+                        border={`2px dashed ${palette.primary.main}`}
+                        p="1rem"
+                        sx={{ "&:hover": { cursor: "pointer" } }}
+                      >
+                        <input {...getInputProps()} />
+                        {!values.picture ? (
+                          <p>Add Picture Here</p>
+                        ) : (
+                          <FlexBetween>
+                            <Typography>{values.picture.name}</Typography>
+                            <EditOutlinedIcon />
+                          </FlexBetween>
+                        )}
+                      </Box>
+                    )}
+                  </Dropzone>
+                </Box>
+              </>
+            )}
 
-                        />
-                        </>
-                  )}
+            <TextField
+              label="Email"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.email}
+              name="email"
+              error={Boolean(touched.email) && Boolean(errors.email)}
+              helperText={touched.email && ErrorSharp.email}
+              sx={{ gridColumn: "span 4" }}
+            />
+            <TextField
+              label="Password"
+              type="password"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.password}
+              name="password"
+              error={Boolean(touched.password) && Boolean(errors.password)}
+              helperText={touched.password && ErrorSharp.password}
+              sx={{ gridColumn: "span 4" }}
+            />
+          </Box>
 
-            </Box>
-            </form>}
+
+          {/**Buttons */}
+        </form>
+      )}
     </Formik>
   );
 };
